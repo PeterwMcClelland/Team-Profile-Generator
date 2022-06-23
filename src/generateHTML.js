f6c2d857eeee2f1db3a9422e791a8e4a498a7d28
Peter McClelland <peterwmcclelland@gmail.com>
2022-06-23 18:11:44 -0500

    Clean

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,7 +1,7 @@
 const generateManager = function (manager) {
     return  `<div class="card-header">
     <h3>${manager.name}</h3>
-    <h4>Manager</h4><i class="material-icons">content_paste</i>
+    <h4>Manager</h4>
 </div>
 <div class="card-body">
     <p class="id">ID: ${manager.id}</p>
@@ -14,7 +14,7 @@ const generateEngineer = function (engineer) {
     return `
     <div class="card-header">
     <h3>${engineer.name}</h3>
-    <h4>Engineer</h4><i class="material-icons"></i>
+    <h4>Engineer</h4>
 </div>
 <div class="card-body">
     <p class="id">ID: ${engineer.id}</p>
